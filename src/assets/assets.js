8b3c7d96d6a99fb4be04f6bf421dc6ce93d02000
Muskan-Musa-Khan <reexoo52@gmail.com>
2025-12-18 16:31:20 +0530

    assets added

---
+++ src/assets/assets.js
@@ -0,0 +1,49 @@
+import comingsoon from "./comingsoon.png";
+import HTML from "./HTML.svg";
+import css from "./CSS.svg"
+import javascript from "./JavaScript.svg";
+import bootstap from "./Bootstrap.svg";
+import Tailwind from "./TailwindCSS.svg";
+import figma from "./Figma.svg";
+import python from "./Python.svg";
+import java from "./java.svg";
+import react from "./react.svg";
+import Git from "./Git.svg";
+import github from "./github.svg";
+import cplus from "./C++.svg";
+import Nextjs from "./Next.js.svg";
+
+export const projectsData = [
+  {
+    img: comingsoon,
+    title: "Project Title",
+    desc: "Developed an animated portfolio website using Next.js with smooth interactive effects and responsive design.",
+  },
+  {
+    img: comingsoon,
+    title: "Project Title",
+    desc: "Developed an animated portfolio website using Next.js with smooth interactive effects and responsive design.",
+  },
+  {
+    img: comingsoon,
+    title: "Project Title",
+    desc: "Developed an animated portfolio website using Next.js with smooth interactive effects and responsive design.",
+  },
+];
+
+export const allSvgs = [
+  { img: HTML, title: "HTML" },
+  { img: css, title: "CSS" },
+  { img: javascript, title: "JavaScript" },
+  { img: bootstap, title: "Bootstrap" },
+  { img: Tailwind, title: "Tailwind" },
+  { img: react, title: "React" },
+  { img: figma, title: "Figma" },
+  { img: Git, title: "Git" },
+  { img: github, title: "Github" },
+
+  { img: Nextjs, title: "Nextjs" },
+  { img: cplus, title: "C++" },
+  { img: python, title: "Python" },
+  { img: java, title: "Java" },
+];

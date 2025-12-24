d0fe5a73c841d1856c1fae8db0e8dc35f9e5febf
Muskan-Musa-Khan <reexoo52@gmail.com>
2025-12-24 11:06:35 +0530

    feat(app): add initial React app with Contact component and theme support

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,31 @@ import React, { useContext } from "react";
 import Navbar from "./components/Navbar";
 import Home from "./sections/Home";
 import About from "./sections/About";
+import { ThemeContext } from "./context/ThemeContext";
 import Projects from "./sections/Projects";
 import Education from "./sections/Education";
 import Skills from "./sections/Skills";
 import Contact from "./sections/Contact";
-import { Routes, Route } from "react-router-dom";
-import { ThemeContext } from "./context/ThemeContext";
+import { ToastContainer } from "react-toastify";
 
 const App = () => {
   const { theme } = useContext(ThemeContext);
   return (
     <div
-      className={`h-screen overflow-hidden w-screen  pt-10
-        ${theme === "dark" ? "bg-black text-white" : "bg-white text-black"}`}
+      className={`h-absolute overflow-hidden w-screen 
+        ${
+          theme === "dark" ? "bg-black dark:text-white " : "bg-white text-black"
+        }
+       `}
     >
       <Navbar />
-
-      <div>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/education" element={<Education />} />
-          <Route path="/skills" element={<Skills />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </div>
+      <ToastContainer />
+      <Home />
+      <About />
+      <Projects />
+      <Education />
+      <Skills />
+      <Contact />
     </div>
   );
 };
